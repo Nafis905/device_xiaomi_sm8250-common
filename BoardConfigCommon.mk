51d8aaf86a8a261bfe5f5c191e8dd33c45c5908b
Dhina17 <dhinalogu@gmail.com>
2025-02-21 07:29:22 +0000

    sm8250-common: Implement torch light control Change-Id: Icd32d1f6aedb55462c9df4d7cc63a2a4c4e4263e
    [RealJohnGalt:] modify for oneplus usage, add toggle switch
    [cyberknight777]: Adapt nodes and SEPolicy for sunny
    [Sanjivns]: Adapt LED nodes for alioth
    
    Change-Id: I0b86fbeefbe3b951c5b9827a06c066dd71ada002
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    Signed-off-by: Sanjivns <alshahab109@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -63,6 +63,7 @@ TARGET_NO_BOOTLOADER := true
 
 # Camera - Miui
 $(call soong_config_set,camera,package_name,com.android.camera)
+TARGET_CAMERA_SERVICE_EXT_LIB := //$(COMMON_PATH):libcameraservice_extension.sm8250
 
 # Display
 TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS := 0x546C00000000
